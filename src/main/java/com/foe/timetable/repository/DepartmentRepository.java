666090b77bf5e5ae7cf041caecce413f4dd1d8fc
Gimaya De Mal <gimaya222@gmail.com>
2026-06-27 09:04:06 +0530

    Remove backend project files from main branch

--- src/main/java/com/foe/timetable/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foe.timetable.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.foe.timetable.model.Department;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-}
